17ee0b42585b04f379b6cb32e1bb25ea6b23a440
Aksel96 <akselvillela@example.com>
2026-02-01 10:39:06 -0600

    maria

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path' // Importante importar esto
 
 export default defineConfig({
-
-    base: '/aksel96.github.io.git',
+    // Recuerda que aquí va el nombre de tu repo, ej: '/aksel96.github.io/'
+    base: '/aksel96.github.io/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'), // Tu portafolio principal
+                maria: resolve(__dirname, 'maria/index.html') // La nueva página
+            }
+        }
+    }
 })
